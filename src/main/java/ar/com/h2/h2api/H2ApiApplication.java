7be643361e6815cc5c0b8e9f339af4b284426017
LAYALA09 <ayalilian2018@gmail.com>
2022-07-15 15:25:38 -0300

    prueba erro

--- src/main/java/ar/com/h2/h2api/H2ApiApplication.java
+++ src/main/java/ar/com/h2/h2api/H2ApiApplication.java
@@ -1,14 +1,19 @@
 package ar.com.h2.h2api;
 
+import ar.com.h2.h2api.entities.Book;
+import ar.com.h2.h2api.repository.BookRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class H2ApiApplication {
 
 	public static void main(String[] args)
 	{
-		SpringApplication.run(H2ApiApplication.class, args);
+		ApplicationContext context=SpringApplication.run(H2ApiApplication.class, args);
+		context.getBean(BookRepository.class);
+		BookRepository repository=context.getBean(BookRepository.class);
 	}
 
 }
---
+++ src/main/java/ar/com/h2/h2api/README.md
@@ -0,0 +1,7 @@
+## appi sencilla ejecutando h2 y swagger
+*H2
+*Spring Data JPA
+*Spring Web
+*Spring Boot Dev tools
+Aplication API REST with acces to bd H2
+Access can be made  from postamn or navigation 
