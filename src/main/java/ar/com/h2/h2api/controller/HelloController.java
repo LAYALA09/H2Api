d3f8d5a0c8dd7e7a514a745bce32af39631d32a0
LAYALA09 <ayalilian2018@gmail.com>
2022-07-30 19:51:37 -0300

    Empezando CRUD con swagger y h2

--- src/main/java/ar/com/h2/h2api/controller/HelloController.java
+++ src/main/java/ar/com/h2/h2api/controller/HelloController.java
@@ -1,7 +1,7 @@
 package ar.com.h2.h2api.controller;
 
 
-import jdk.nashorn.internal.runtime.linker.Bootstrap;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +16,6 @@ public class HelloController {
     @GetMapping("/bootstrap")
     public String bootstrap() {
 
-        return null;
+return null;
     }
-                
-
 }
